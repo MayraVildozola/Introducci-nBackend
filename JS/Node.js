f1acaac379b03b39715b4af446b726856947cbf1
Mayra Gisela Vildozola Sigala <mayravildozola@hotmail.com>
2022-04-28 20:24:41 -0600

    Ingresar errores

--- JS/Node.js
+++ JS/Node.js
@@ -36,3 +36,5 @@ http.createServer(function (request, response){
                 response.end(content,'utf-8');
             }
         });
+    }).listen(3000);
+    console.log('Server running at http://127.0.0.1:3000/');
