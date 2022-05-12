88cae6a3faeeef469f9593f425141d8f6b356d4c
Mayra Gisela Vildozola Sigala <mayravildozola@hotmail.com>
2022-05-12 11:30:39 -0600

    Actualizar un usuario exixtente

--- routes.js
+++ routes.js
@@ -34,3 +34,13 @@ app.post(`/users`, (request, response) => {
         response.status(201).send(`User added with ID: ${result.insertId}`);
     });
 }):
+//Actualizar un usuario exixtente
+app.put(`/users/:id`, (request, response) =>{
+    const id = request.params.id;
+
+    pool.quary(`UPDATE users SET ? WHERE id = ?`, [request.body, id],(error, result) =>{
+        if (error) throw error;
+
+        response.send(`User updated successfully.`);
+    });
+});
